819833af5661b1d7f2cf68b78c088d9510a7471d
Gustavo Lima <eng.lima1911@gmail.com>
2025-02-10 10:35:49 -0300

    Black

--- Códigos/__main__.py
+++ Códigos/__main__.py
@@ -34,6 +34,4 @@ if __name__ == "__main__":
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    process_file(
-        input_file, output_file
-    )
+    process_file(input_file, output_file)
